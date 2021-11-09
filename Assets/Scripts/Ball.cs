3a129c52f1bde7577326ca14694d03b2a7e22a02
ddoddodododdo <rkdtks6128@gmail.com>
2021-11-10 00:00:56 +0900

    공 판정 수정

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,27 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviourPunCallbacks, IPunObservable
 {
     public Rigidbody rigid;
 
     private const string playerTag = "Player";
     private const string wallTag = "Wall";
 
-    void Start()
-    {
-        
-    }
-
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.transform.CompareTag(playerTag))
         {
-            rigid.velocity = col.rigidbody.velocity * 3;
+            photonView.RPC("AddForce", RpcTarget.MasterClient, col.rigidbody.velocity);
         }
 
     }
+
+    [PunRPC]
+    public void AddForce(Vector3 force)
+    {
+        rigid.velocity = force * 3;
+    }
+
+
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting && PhotonNetwork.IsMasterClient)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(rigid.velocity);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            rigid.velocity = (Vector3)stream.ReceiveNext();
+        }
+    }
+
 
 }
